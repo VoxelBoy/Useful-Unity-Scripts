116ab5ac6e13d926757b14c9e3e44054d7cc71b7
Yilmaz Kiymaz <ylmzkymz@gmail.com>
2015-12-15 17:09:18 +0100

    Update InspectorShortcuts.cs

--- InspectorShortcuts.cs
+++ InspectorShortcuts.cs
@@ -4,18 +4,18 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 
-public class InspectorShortcuts
+public static class InspectorShortcuts
 {
 
-    public static BindingFlags fullBinding = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
-    public static StringComparison ignoreCase = System.StringComparison.CurrentCultureIgnoreCase;
+	public static BindingFlags FullBinding = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+	public static StringComparison IgnoreCase = System.StringComparison.CurrentCultureIgnoreCase;
 
-    private enum InspectorModesToUse
-    {
-        Normal = 0,
-        Debug = 1,
-        //DebugInternal = 2,
-    }
+	private enum InspectorModesToUse
+	{
+		Normal = 0,
+		Debug = 1,
+		//DebugInternal = 2,
+	}
 
 	static Assembly unityEngineAssembly;
 	static Type inspectorWindowType;
@@ -41,11 +41,11 @@ public class InspectorShortcuts
 			return false;
 		}
 
-		allInspectorsFieldInfo = inspectorWindowType.GetField("m_AllInspectors", fullBinding);
-		inspectorModeFieldInfo = inspectorWindowType.GetField("m_InspectorMode", fullBinding);
-		setModeMethodInfo = inspectorWindowType.GetMethod("SetMode", fullBinding);
-		flipLockedMethodInfo = inspectorWindowType.GetMethod("FlipLocked", fullBinding);
-		repaintAllInspectorsMethodInfo = inspectorWindowType.GetMethod("RepaintAllInspectors", fullBinding);
+		allInspectorsFieldInfo = inspectorWindowType.GetField("m_AllInspectors", FullBinding);
+		inspectorModeFieldInfo = inspectorWindowType.GetField("m_InspectorMode", FullBinding);
+		setModeMethodInfo = inspectorWindowType.GetMethod("SetMode", FullBinding);
+		flipLockedMethodInfo = inspectorWindowType.GetMethod("FlipLocked", FullBinding);
+		repaintAllInspectorsMethodInfo = inspectorWindowType.GetMethod("RepaintAllInspectors", FullBinding);
 		if (allInspectorsFieldInfo == null || inspectorModeFieldInfo == null || setModeMethodInfo == null || flipLockedMethodInfo == null || repaintAllInspectorsMethodInfo == null)
 		{
 			return false;
@@ -54,24 +54,24 @@ public class InspectorShortcuts
 		return true;
 	}
 
-    [MenuItem("Window/Toggle Inspector Mode #&d")]
-    public static void ToggleInspectorMode()
-    {
+	[MenuItem("Window/Toggle Inspector Mode #&d")]
+	public static void ToggleInspectorMode()
+	{
 		bool init = Init();
 		if(!init)
 		{
 			return;
 		}
-        
-        IList allInspectorsList = (IList)allInspectorsFieldInfo.GetValue(null);
+		
+		var allInspectorsList = (IList)allInspectorsFieldInfo.GetValue(null);
 
-        foreach (object inspectorWindow in allInspectorsList)
-        {
-            int currentMode = (int)inspectorModeFieldInfo.GetValue(inspectorWindow);
-            int newMode = (currentMode + 1) % System.Enum.GetValues(typeof(InspectorModesToUse)).Length;
-            setModeMethodInfo.Invoke(inspectorWindow, new object[] { newMode });
-        }
-    }
+		foreach (object inspectorWindow in allInspectorsList)
+		{
+			int currentMode = (int)inspectorModeFieldInfo.GetValue(inspectorWindow);
+			int newMode = (currentMode + 1) % System.Enum.GetValues(typeof(InspectorModesToUse)).Length;
+			setModeMethodInfo.Invoke(inspectorWindow, new object[] { newMode });
+		}
+	}
 
 	[MenuItem("Window/Toggle Inspector Lock #&l")]
 	public static void ToggleInspectorLock()
@@ -82,7 +82,7 @@ public class InspectorShortcuts
 			return;
 		}
 
-		IList allInspectorsList = (IList)allInspectorsFieldInfo.GetValue(null);
+		var allInspectorsList = (IList)allInspectorsFieldInfo.GetValue(null);
 
 		foreach (object inspectorWindow in allInspectorsList)
 		{
@@ -91,17 +91,17 @@ public class InspectorShortcuts
 		repaintAllInspectorsMethodInfo.Invoke(null, null);
 	}
 
-    public static Type GetTypeFromAssembly(string typeName, Assembly assembly, bool exactNameMatch = false)
-    {
-        Type[] types = assembly.GetTypes();
-        foreach (Type type in types)
-        {
-            if (exactNameMatch && type.FullName == typeName)
-                return type;
+	public static Type GetTypeFromAssembly(string typeName, Assembly assembly, bool exactNameMatch = false)
+	{
+		var types = assembly.GetTypes();
+		foreach (Type type in types)
+		{
+			if (exactNameMatch && type.FullName == typeName)
+				return type;
 
-            if (type.Name.Equals(typeName, ignoreCase) || type.Name.Contains('+' + typeName)) //+ checks for inline classes
-                return type;
-        }
-        return null;
-    }
+			if (type.Name.Equals(typeName, IgnoreCase) || type.Name.Contains('+' + typeName)) //+ checks for inline classes
+				return type;
+		}
+		return null;
+	}
 }
